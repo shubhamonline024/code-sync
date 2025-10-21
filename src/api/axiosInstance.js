96491e72504e7aab03b684394590c936fddc9db6
Shubham Shigwan <shubham.shigwan@ext.quantiphi.com>
2025-10-21 23:03:15 +0530

    wrk

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const backendURL = import.meta.env.VITE_API_BASE_URL;
+console.log("backend URL -> ", backendURL);
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3001",
+  baseURL: backendURL || "http://localhost:3001",
   //   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
