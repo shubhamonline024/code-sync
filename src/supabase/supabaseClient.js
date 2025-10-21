6cf0c6fd4dfbf93f0ec4f2bff852904641914f50
Shubham Shigwan <shubham.shigwan@ext.quantiphi.com>
2025-10-21 21:14:06 +0530

    corrections

--- src/supabase/supabaseClient.js
+++ src/supabase/supabaseClient.js
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = process.env.SUPABASE_URL;
-// const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
-
-const supabaseUrl = "https://glpiovrqaarqchrxjths.supabase.co";
-const supabaseAnonKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdscGlvdnJxYWFycWNocnhqdGhzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ3NTExNTcsImV4cCI6MjA3MDMyNzE1N30.FNzrzl0sUfNiv3G7UlAntJ-UdyxRzv_t0LBembz6VCk";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
